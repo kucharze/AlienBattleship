eac83e33a71c6fa324c19a9f95032256e378c1d0
Zachary Kuchar <zacharyk323@live.com>
2023-06-20 15:40:32 -0400

    Adjusted console logs, added new functions

--- app.js
+++ app.js
@@ -71,17 +71,18 @@ let round = 0;
 
 //Function attack player
 const attackPlayer = (player, attacker) => {
-  console.log(player.hull);
+  //   console.log(player.hull);
   //determine accuracy and firepower and see if hit landed
   let accuracy = Math.floor(Math.random() * (10 - 1) + 1);
-  console.log("value", accuracy);
-  console.log("attacker accuracy", attacker.accuracy);
+  //   console.log("value", accuracy);
+  //   console.log("attacker accuracy", attacker.accuracy);
   if (accuracy <= attacker.accuracy) {
     player.hull -= attacker.firepower;
+    console.log("The shot hit", player.hull);
   } else {
     console.log("The shot missed");
   }
-  console.log(player.hull);
+  //   console.log(player.hull);
 };
 
 //function to determine if your attack works
@@ -98,31 +99,49 @@ const shootAlien = () => {
   attackPlayer(aliens[round], playerShip);
 
   if (aliens[round].hull > 0) {
-    console.log("The alien readies a shot");
+    console.log(aliens[round].name, "readies a shot");
     attackPlayer(playerShip, aliens[round]);
   } else {
     console.log("The alien is defeated!!!");
     //Determine if there are still aliens left
-    if (round == 5) {
+    if (round === 5) {
       //If so give option to retreat or stay
     } else {
       //If not say you win
       //Increase round by 1
+      console.log("Continue on?");
+      continueOn();
     }
   }
 };
 
 //Function for determining continue or retreat
-const continueOn = () => {};
+const continueOn = () => {
+  let options = document.querySelector(".continueOn");
+  options.style = "display:block";
+};
 
 //Display player or alien
-const display = () => {};
+const display = () => {
+  console.log(aliens[round].name, "is approaching");
+};
 
 //Game start up
 const startUp = () => {
-  console.log("");
+  console.log("The aliens are attacking");
 };
 
+//Function to continue
+const moveOn = () => {
+  round++;
+  let options = document.querySelector(".continueOn");
+  options.style = "display:none";
+};
+
+//Function to escape
+const escape = () => {
+  console.log("You have escaped, you killed", round, "alien ships");
+};
 //win state(all ships destroyed)
 
 //lose state(you are destroyed)
@@ -134,3 +153,4 @@ const startUp = () => {
 //main game loop
 //You attack alien, than determine if it is alive
 //If alive, alien attacks you and then determin if you survive
+startUp();
