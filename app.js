7da51d17537e2ca1965eb9233881bb27d29437f7
Zachary Kuchar <zacharyk323@live.com>
2023-06-20 10:39:38 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//A game round
+// You attack the first alien ship
+// If the ship survives, it attacks you
+// If you survive, you attack the ship again
+// If it survives, it attacks you again ... etc
+// If you destroy the ship, you have the option to attack the next ship or to retreat
+// If you retreat, the game is over, perhaps leaving the game open for further developments or options
+// You win the game if you destroy all of the aliens
+// You lose the game if you are destroyed
+
+// There are six alien ships.
+// The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship.
+// Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order.
+// After you have destroyed a ship, you have the option to make a hasty retreat.
+
+//The player ship
+
+//List of alien ships
+
+//main game loop
+
+//function to determine if your attack works
+
+//function to determine if alien attack works
